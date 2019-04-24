2398451b2d888f091691c9f71edb7256bc2597cb
suresh-kansujiya <sureshkansujiya@gmail.com>
2019-04-24 15:06:36 +0530

    initial commit
    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://sureshkansujiya:'+ encodeURIComponent('Sari#9414913669') + '@cluster0-pst9e.mongodb.net/test?retryWrites=true';
+//mongodb+srv://sureshkansujiya:<password>@cluster0-pst9e.mongodb.net/test?retryWrites=true
+module.exports = {
+    mongoURI: dbPassword
+};
